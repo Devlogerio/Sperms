9766862997c5e2bed4723aded3e8b32f11b7319f
Cena Abachi <devloger.io@gmail.com>
2023-09-01 23:12:45 +0400

    First version - remaking after lose of data
    Sperms game no ending, no menu, global vars need change

---
+++ Sperms/globalVars.js
@@ -0,0 +1,49 @@
+// General
+var isPaused = true;
+
+//road and map
+var lastWidthSize;
+var lastHeightSize;
+var roadLeft = [];
+var roadRight = [];
+var roadMiddle = [];
+var roadStrokeWeight = 1;
+var mapWidth = 800;// 500
+var mapHeight = 1500;// 500
+var roadWidth = 15;
+var leftRightDiffrance = 0;
+var roadEndingY = 240;
+var roadSidesForce = 0.28;
+var roadHeight = 0;
+var roadsideSqueeshinessMultiplier = 0.5;
+
+//sperms
+var sperms = [];
+var uterus;
+var ovum;
+var globalVelocityLimit = 0.7;
+var globalMaxveForce = 0.16;
+var globalNPCVelocityLimit = 1.0;
+var globalNPCMaxveForce = 0.06;
+var jelliesSlowDownNPC = 0.8;
+var NPCJammersSlowedDown = 2.6;
+var jammersForce = 0.2;
+var jelliesForce = -0.05;
+var uterusForce = 0.3;
+var spermPupolation = 5; //50
+var NPC_sperm_steering_accuracy = 1;
+
+//Entities
+var jammers = [];
+var jellies = [];
+var leftAlgas = [];
+var rightAlgas = [];
+var jammersCrowd = mapHeight / 10;// 25
+var jelliesCrowd = mapHeight / 5;// 100
+var algasCrowd = mapHeight;// 500
+
+//graphics
+var graphicQuality = 'low';
+var gameScale = 20;
+var staticGameScale = 20;
+var viewDisance = 40;
---
+++ Sperms/utrus.js
@@ -0,0 +1,31 @@
+
+
+var Uterus = function () {
+  var self = Entity();
+  self.r = 100;
+  self.rr = self.r * 2;
+  //self.strokeColor = color(255, 255, 255,255);
+  self.color = color(164, 64, 74, 255);
+
+  //Positioning the sperm at the begining
+  self.putInLocation = function () {
+    var yPoint = 0;
+    var d = dist(roadLeft[yPoint].x, roadLeft[yPoint].y, roadRight[yPoint + leftRightDiffrance].x, roadRight[yPoint + leftRightDiffrance].y);
+    self.location.x = roadLeft[yPoint].x - floor(d);
+    self.location.y = roadLeft[yPoint].y - self.r + 5;
+  }
+
+  self.draw = function () {
+    push();
+    //setup
+    translate(self.location.x, self.location.y);
+    //strokeWeight(0.5);
+    //stroke(self.strokeColor);
+    noStroke();
+    fill(self.color);
+    //draw body
+    ellipse(0, 0, self.rr, self.rr);
+    pop();
+  }
+  return self;
+}
