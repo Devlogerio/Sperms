7ceafedf38cc3fe64982c4b0cf01cab82e1254b6
Cena Abachi <devloger.io@gmail.com>
2023-09-09 22:21:01 +0400

    Update jammer.js

--- jammer.js
+++ jammer.js
@@ -1,4 +1,4 @@
-
+// Made by Cena Abachi Known as Devlogerio, find me on Youtube, Instagram, and Github: Devlogeiro LinkedIn: Cena Abachi, devloger.io@gmail.com
 var Jammer = function () {
   var self = Entity();
   self.r = 2;
