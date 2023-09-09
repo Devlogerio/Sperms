fcece5aad49e4ea74f8fe1f16131167ecd45d4fd
Cena Abachi <devloger.io@gmail.com>
2023-09-09 22:21:52 +0400

    Update uvom.js

--- uvom.js
+++ uvom.js
@@ -1,4 +1,4 @@
-
+// Made by Cena Abachi Known as Devlogerio, find me on Youtube, Instagram, and Github: Devlogeiro LinkedIn: Cena Abachi, devloger.io@gmail.com
 
 var Ovum = function () {
     var self = Entity();
